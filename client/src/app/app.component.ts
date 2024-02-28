5554a55b9429fc579276a5225f041212122884b9
Marc <ninjaz.gg@outlook.com>
2024-02-28 14:08:20 -0500

    init web ui

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, HomeComponent]
+})
+export class AppComponent {
+  title = 'kyara';
+}
